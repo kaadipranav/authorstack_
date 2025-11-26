45b431cd1992a20acb78447257b4495eb747be36
kaadipranav <kiwi092020@gmail.com>
2025-11-26 19:59:56 +0530

    favicon added

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,14 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait-primary",
     icons: [
       {
-        src: "/logos/Light_logo.png",
+        src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: "/logos/Light_logo.png",
+        src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
